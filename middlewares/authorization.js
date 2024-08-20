0f9b4251e1fbd3ff86081d427624b2401ab5e621
Abobakr <mabobakr79@yahoo.com>
2024-08-20 21:50:36 +0300

    feat: added authorization middleware

---
+++ middlewares/authorization.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const ApiError = require("../utils/error/ApiError");
+const asyncHandler = require("../utils/error/asyncHandler");
+
+const auth = asyncHandler(async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        throw new ApiError(401, "Please authenticate");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, (err) => {
+        if (err) {
+            throw new ApiError(401, "Invalid token");
+        }
+        return jwt.decode(token);
+    });
+    const user = await User.findById(decoded._id);
+    if (!user) {
+        throw new ApiError(401, "Please authenticate");
+    }
+    req.user = user;
+    next();
+});
