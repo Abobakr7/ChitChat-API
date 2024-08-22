a2bacf8904f3f053e66c6e38f3f9518942fa1634
Abobakr <mabobakr79@yahoo.com>
2024-08-22 18:00:11 +0300

    feat: added multer configuration

---
+++ utils/multer.js
@@ -0,0 +1,16 @@
+const multer = require("multer");
+const ApiError = require("./error/ApiError");
+
+module.exports = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        if (ext !== "jpeg" && ext !== "jpg" && ext !== "png") {
+            return cb(
+                new ApiError(400, "Only images jpeg/jpg/png are allowed"),
+                false
+            );
+        }
+        cb(null, true);
+    },
+});
