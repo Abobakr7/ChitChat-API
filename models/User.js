1ba065ded2a03706f518dd810f7310e6ac15a228
Abobakr <mabobakr79@yahoo.com>
2024-08-22 15:53:05 +0300

    refactor: modified pre hook save

--- models/User.js
+++ models/User.js
@@ -42,8 +42,9 @@ const userSchema = new Schema(
 );
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 8);
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
     next();
 });
 
