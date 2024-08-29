55f2bd9db07664d670e0c77b438e3f3d7426e5dd
Abobakr <mabobakr79@yahoo.com>
2024-08-29 18:10:11 +0300

    refactor(database): refactored connection and added disconnect method

--- src/database/mongo-db.js
+++ src/database/mongo-db.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DB_URI);
-mongoose.connection
-    .once("open", () => {
-        console.log("Connected to DB Successfully");
-    })
-    .on("error", (err) => {
-        console.warn("Connection to DB Faild");
-        console.warn("Warning: ", err);
-        process.exit(1);
-    });
+exports.MongoConnect = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log("Connected to MongoDB Successfully");
+    } catch (err) {
+        console.error("Connection to MongoDB Faild");
+        console.error("Warning: ", err);
+        process.kill(process.pid, "SIGTERM");
+    }
+};
+
+exports.MongoDisconnect = async () => {
+    await mongoose.disconnect();
+    console.log("Disconnected from MongoDB Successfully");
+};
