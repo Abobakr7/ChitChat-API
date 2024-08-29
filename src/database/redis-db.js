639d85600c6c75f7ca47a150813963b9ae7b376f
Abobakr <mabobakr79@yahoo.com>
2024-08-29 18:19:46 +0300

    feat(database): added connect & disconnect to redis

---
+++ src/database/redis-db.js
@@ -0,0 +1,23 @@
+const { createClient } = require("redis");
+
+const client = createClient({ url: process.env.REDIS_URI });
+client.on("error", (error) => {
+    console.error("Redis Client Error: ", error);
+    process.kill(process.pid, "SIGTERM");
+});
+
+exports.RedisConnect = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to Redis Successfully");
+    } catch (err) {
+        console.error("Connection to Redis Faild");
+        console.error("Warning: ", err);
+        process.kill(process.pid, "SIGTERM");
+    }
+};
+
+exports.RedisDisconnect = async () => {
+    await client.disconnect();
+    console.log("Disconnected from Redis Successfully");
+};
